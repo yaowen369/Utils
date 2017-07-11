25bea9df77e1502db44ee263a93ecab5f5e29061
yaowen369 <yaowen369@163.com>
2017-07-11 19:12:59 +0800

    晚上自定义ViewGroup

--- sample/src/main/java/com/yaoxiaowen/android/sample/view/FlowLayout.java
+++ sample/src/main/java/com/yaoxiaowen/android/sample/view/FlowLayout.java
@@ -5,11 +5,15 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.yaoxiaowen.android.sample.log.LogUtils;
+
 /**
  * Created by Administrator on 2017/7/10.
  */
 
 public class FlowLayout extends ViewGroup{
+
+    private static final String TAG = "FlowLayout";
 
     private Context mContext;
 
@@ -43,14 +47,113 @@ public class FlowLayout extends ViewGroup{
 
 //        final LayoutParams mLp = getLayoutParams();
 
-        int count = getChildCount();
-        for (int i=0; i<count; i++){
+//        ViewGroup.LayoutParams lp = getLayoutParams();
 
-        }
+        //如果我想使用  MarginLayoutParams, 则必须 我们自己的att,来获取他
+//        ViewGroup.MarginLayoutParams lp = getLayoutParams();
+
+
+//        final int count = getChildCount();
+//        for (int i=0; i<count; i++){
+//            final View view = getChildAt(i);
+//            if (view.getVisibility() == GONE){
+//                continue;
+//            }else {
+////                measureChildWithMargins(view, widthMeasureSpec, 0, heightMeasureSpec, 0);
+//                measureChild(view, widthMeasureSpec, heightMeasureSpec );
+//            }
+//        }
+
+
+
 
         measureChildren(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec),
-                getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec));
+
+        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+
+        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        //宽度要特殊处理
+        int oneLineMaxWidth = 0;    // 所有行当中，最宽的某一行
+        int childrenWidthSum = 0;   //某一行 宽度的总和
+        int childrenHeightSum = 0;  // 每行加起来高度的总和
+        int oneLineMaxHeight = 0;  //某一行的最大高度
+
+
+        // 计算各个子View在  每行排列当中高度总和，以及最大宽度的情况
+        final int count = getChildCount();
+        for (int i=0; i<count ;i++){
+            final  View child = getChildAt(i);
+
+            if ((childrenWidthSum + child.getMeasuredWidth()) > widthSpecSize){ //这一行 已经满了，需要另起一行
+                childrenHeightSum += oneLineMaxHeight;
+                childrenWidthSum = 0;
+                oneLineMaxHeight = 0;
+            }
+
+            childrenWidthSum += child.getMeasuredWidth();
+            if (childrenWidthSum > oneLineMaxWidth){
+                oneLineMaxWidth = childrenWidthSum;
+            }
+
+            if (child.getMeasuredHeight() > oneLineMaxHeight){
+                oneLineMaxHeight = child.getMeasuredHeight();
+            }
+        }
+        childrenHeightSum += oneLineMaxHeight;
+
+        StringBuilder calcResult = new StringBuilder();
+        calcResult.append("oneLineMaxWidth = " + oneLineMaxWidth)
+                .append("\t childrenWidthSum = " + childrenWidthSum)
+                .append("\t childrenHeightSum = " + childrenHeightSum)
+                .append("\t oneLineMaxHeight= " + oneLineMaxHeight);
+
+        LogUtils.i(TAG, "calcResult = " + calcResult.toString());
+
+        //考虑 wrap_content的情况
+        int resultWidth = -1;
+        int resultHeight = -1;
+
+        if (widthSpecMode==MeasureSpec.AT_MOST && widthSpecMode==MeasureSpec.AT_MOST){
+            resultHeight = childrenHeightSum;
+            resultWidth = oneLineMaxWidth;
+        }else if (widthSpecMode==MeasureSpec.AT_MOST){
+            resultWidth = oneLineMaxWidth;
+        }else if (widthSpecMode==MeasureSpec.AT_MOST){
+            resultHeight = childrenHeightSum;
+        }
+
+
+        StringBuilder result = new StringBuilder();
+        result.append("\t resultHeight=" + MeasureSpec.toString(resultHeight))
+                .append("\t resultWidth=" + MeasureSpec.toString(resultWidth));
+
+        LogUtils.i(TAG, "result = " + result.toString());
+
+
+        if (resultHeight < 0){
+            resultHeight = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+        }
+
+        if (resultWidth < 0){
+            resultWidth = getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        }
+
+        //for test
+        StringBuilder sb = new StringBuilder();
+        sb.append("widthMeasureSpec=" + MeasureSpec.toString(widthMeasureSpec))
+                .append("\t heightMeasureSpec" + MeasureSpec.toString(heightMeasureSpec))
+                .append("\t resultHeight=" + MeasureSpec.toString(resultHeight))
+                .append("\t resultWidth=" + MeasureSpec.toString(resultWidth));
+
+        LogUtils.i(TAG, sb.toString());
+
+        setMeasuredDimension(resultWidth, resultHeight);
+
+//        setMeasuredDimension(getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec),
+//                getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec));
 
     }
 
